bcc3fcf0bbed34e09e97d139df0b3e9c76eb5cfa
Shivraj koli <kolishivraj05@gmail.com>
2022-11-26 15:00:11 +0530

    Add nodejs app files

---
+++ app/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const os = require("os");
+const hostname = os.hostname();
+
+app.get("/", (req, res) => res.send("Hello from " + hostname));
+app.listen(8081, () => console.log("listening on port 8081 on " + 
+hostname));
